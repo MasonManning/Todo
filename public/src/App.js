a33153c7d52961c289f5bfd93bec879ba416ff47
MasonManning <MasonManning@protonmail.com>
2020-08-22 03:47:59 +1000

    Retrieve api todo items from node api and display in list

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    fetch('/api/todo')
+    .then(res => res.json())
+    .then(data => {
+      setIsLoading(false)
+      setTodos(data)
+    })
+    console.log("Use Effect")
+  }, [])
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <h1>{!isLoading ? "Completed" :  "Lading..."}</h1>
+      {!isLoading ? todos.map(todo=> <TodoItem key={todo._id} todo={todo.todo} isDone={todo.isDone}/>): "loading"}
       <TodoItem todo='work out' isDone={true}/>
     </div>
   );
