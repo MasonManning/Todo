a9af131637b22d50562e25ed0eb6e77d7cf01d9e
MasonManning <MasonManning@protonmail.com>
2020-08-27 23:54:17 +1000

    Consume api endpoint to save checkbutton state on server

--- public/src/components/TodoItem.js
+++ public/src/components/TodoItem.js
@@ -18,17 +18,31 @@ function TodoItem(props) {
         })
             .then(data => data.status === 200 ? props.deleteTodo(props.id) : '')
     }
+    const handleChange = (event) => {
+        fetch('/api/todo', {
+            method: 'POST',
+            headers: {
+                'Accept': 'Application/json',
+                'Content-Type': 'Application/json',
+            },
+            body: JSON.stringify({
+                id: props.id,
+                todo: props.todo,
+                isDone: event.target.checked 
+            })
+        })
+    }
     return (
         <Alert variant='primary' >
             <Row>
-                <Col md={{span:7}}>
+                <Col md={{ span: 7 }}>
                     <h3>{props.todo}</h3>
                 </Col>
                 <Col md="2">
-                    <input type="checkbox" defaultChecked={props.isDone} />
+                    <input type="checkbox" defaultChecked={props.isDone} onChange={handleChange} />
                 </Col>
-                <Col md={{span:1, offset:2}}>
-                    <Button  onClick={handleDelete} variant='danger'>X</Button>
+                <Col md={{ span: 1, offset: 2 }}>
+                    <Button onClick={handleDelete} variant='danger'>X</Button>
                 </Col>
             </Row>
         </Alert>
