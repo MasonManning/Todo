6baa2d6c70111272ffedaaa0b18e147fda9a18b5
MasonManning <MasonManning@protonmail.com>
2020-08-29 22:28:21 +1000

    Add Edit functionality

--- public/src/components/TodoItem.js
+++ public/src/components/TodoItem.js
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col'
 function TodoItem(props) {
     const [editFlag, setEditFlag] = useState(false)
     const [editTodo, setEditTodo] = useState(props.todo)
+    const [isDone, setIsDone] = useState(props.isDone)
     const HIDE = {display: "none"} 
     const SHOW = {display: "block"} 
     const handleDelete = (event) => {
@@ -23,6 +24,7 @@ function TodoItem(props) {
             .then(data => data.status === 200 ? props.deleteTodo(props.id) : '')
     }
     const handleChange = (event) => {
+        setIsDone(event.target.checked)
         fetch('/api/todo', {
             method: 'POST',
             headers: {
@@ -39,22 +41,42 @@ function TodoItem(props) {
     const handleEdit = (event) => {
         console.log(editFlag)
         setEditFlag(ps => !ps)
+        setEditTodo(props.todo)
     }
     const handleEditChange = (event) => {
         console.log(editTodo)
+        setEditTodo(event.target.value)
+    }
+    const handleSave = (event) => {
+        setEditFlag(ps => !ps)
+        fetch('/api/todo', {
+            method: 'POST',
+            headers: {
+                'Accept': 'Application/json',
+                'Content-Type': 'Application/json',
+            },
+            body: JSON.stringify({
+                id: props.id,
+                todo: editTodo,
+                isDone: isDone
+            })
+        })
 
     }
     return (
         <Alert variant='primary' >
             <Row>
-                <Col md={{span:1}}>
+                <Col md={{span:1}} style={editFlag ? HIDE : SHOW}>
                     <Button onClick={handleEdit}>Edit</Button>
+                </Col>
+                <Col md={{span:1}} style={editFlag ? SHOW : HIDE }>
+                    <Button onClick={handleSave}>Save</Button>
                 </Col>
                 <Col md={{ span: 6 }} style={editFlag ? SHOW : HIDE}>
                     <input type="text" value={editTodo} onChange={handleEditChange}/>
                 </Col>
                 <Col md={{ span: 6 }} style={editFlag ? HIDE : SHOW}>
-                    <h3>{props.todo}</h3>
+                    <h3>{editTodo}</h3>
                 </Col>
                 <Col md="2" >
                     <input type="checkbox" defaultChecked={props.isDone} onChange={handleChange} />
